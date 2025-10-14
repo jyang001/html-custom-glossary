f59312ee91d5fca510d190f775ba8711ad9d9370
jyang001 <joeyang095@gmail.com>
2025-10-14 01:00:34 -0400

    Add feature to close sidebar

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const toggleButton = document.getElementById('toggle-btn');
+const sidebarColl = document.getElementsByClassName('sidebar');
+
+let toggleSidebar = (button) => {
+    // TODO: implement 'close' class
+    let sidebar = sidebarColl[0];
+    sidebar.classList.toggle('close');
+    toggleButton.classList.toggle('rotate');
+}
+
 let toggleSubMenu = (button) => {
     button.nextElementSibling.classList.toggle('show');
     button.classList.toggle('rotate');
